29f0a190580e80644659d32e02f47a9748df0194
AriadnaDev <avilavillegasariadna@gmail.com>
2026-04-22 20:11:12 +0200

    Proyecto WordPress Berry Records

---
+++ wordpress/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1776963743;s:7:"authKey";s:64:"UV?Oo>8n(|/9fet)fcg)EXXQ?O>,4;!)ZCqV|`E%,FI>G_)z)#5;N^,+O8kO^,-P";s:7:"version";s:5:"1.1.0";}
